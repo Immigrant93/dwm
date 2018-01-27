8d5398d6fa3edf50f71b7530289b3a6f2700d9d3
Manidakis Marinos <manidakismarinos@gmail.com>
2018-01-27 15:36:51 +0200

    Update to urxvt

--- config.def.h
+++ config.def.h
@@ -49,7 +49,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
 static const char *volupcmd[] = {"amixer", "-D", "pulse", "sset", "Master", "5%+"};
 static const char *voldowncmd[] = {"amixer", "-D", "pulse", "sset", "Master", "5%-"};
 static const char *volmutecmd[] = {"amixer", "-D", "pulse", "sset", "Master", "toggle"};
