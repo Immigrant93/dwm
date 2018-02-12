2344ac132773f031642c0abe3d00da643f7b8fce
Manidakis Marinos <manidakismarinos@gmail.com>
2018-02-12 10:05:12 +0200

    Fix spacing and remove comment

--- config.def.h
+++ config.def.h
@@ -23,12 +23,8 @@ static const char *colors[][3]      = {
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "st", NULL, NULL, 0, 0, -1}
+	{ "st",       NULL,       NULL,       0,            0,           -1}
 };
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
